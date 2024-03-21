895b9f9d4aa8c9c549e52fabe9a8e9df4f046a28
Mafiaaaaaa <ss4113748@gmil.com>
2024-03-22 02:04:24 +0500

    uploading my project

---
+++ main.ts
@@ -0,0 +1,19 @@
+#! /usr /bin/env node
+console.log ("Welcome to Shaheer's Simple Calculator")
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {message: "enter first number", type: "number", name: "firstnumber"},  
+    {message:"enter second number", type:"number", name:"secondnumber"},
+    {message:"operator", type:"list", name:"operator", choices:["Addition", "Subtraction", "Multiplication", "Division"] },
+
+])
+
+if(answer.operator == "Addition"){
+console.log(answer.firstnumber + answer.secondnumber)}
+else if (answer.operator == "Subtraction")
+{console.log(answer.firstnumber - answer.secondnumber)}
+else if (answer.operator == "Multiplication")
+{console.log(answer.firstnumber * answer.secondnumber)}
+else if(answer.operator == "Division")
+{console.log(answer.firstnumber / answer.secondnumber)}
+else {console.log("invalid")}
